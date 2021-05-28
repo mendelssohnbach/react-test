4cd35c362359774447ea5b88fa7bab232ac818d2
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-05-29 06:09:45 +0900

    :+: input初期値1 test: good

--- src/Counter/Counter.jsx
+++ src/Counter/Counter.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react';
 
 function Counter() {
   const [counterValue, setCounterValue] = useState(0);
+  const [inputCValue, setInputCValue] = useState(1);
 
   return (
     <div>
       <h2 data-testid="header">My Counter</h2>
       <h2 data-testid="counter">{counterValue}</h2>
       <button data-testid="subtract-btn">-</button>
+      <input type="number" data-testid="input" value={inputCValue} />
       <button data-testid="add-btn">+</button>
     </div>
   );
