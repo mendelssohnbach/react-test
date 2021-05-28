ee4c04f9b3a2bc19fcf600886b6c9e2fa7c258e8
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-05-29 05:57:47 +0900

    :++1: Couterの初期0 test good

--- src/Counter/Counter.jsx
+++ src/Counter/Counter.jsx
@@ -1,7 +1,12 @@
+import { useState } from 'react';
+
 function Counter() {
+  const [counterValue, setCounterValue] = useState(0);
+
   return (
     <div>
       <h2 data-testid="header">My Counter</h2>
+      <h2 data-testid="counter">{counterValue}</h2>
     </div>
   );
 }
